db4993200669008af5a7caa48020762aa2faff43
njguld95038043 <t.tamura6060@gmail.com>
2022-03-29 01:22:04 +0000

    [Update]ゲストログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
 
+  before_action :ensure_guest_user, only: [:edit]
+
   def guest_sign_in
     user = User.guest
     sign_in user
     redirect_to user_path(user), notice: 'guestuserでログインしました。'
+  end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
   end
 
 end
