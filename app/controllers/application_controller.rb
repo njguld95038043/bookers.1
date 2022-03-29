4bd0bc6cb873d59b76ec9f1f2a71e2e593593144
njguld95038043 <t.tamura6060@gmail.com>
2022-03-29 01:19:51 +0000

    [Add]devise/ゲストログイン機能

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to user_path(user), notice: 'guestuserでログインしました。'
+  end
+
+end
